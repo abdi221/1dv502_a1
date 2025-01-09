cb1587ed8ec0ced8a65ee69981957ebcccaa3e8f
Abdifataah Abdillahi Ali <abdifatahali22@gmail.com>
2025-01-09 20:13:40 +0100

    Initial changes to class

--- app/src/main/java/onedv502_a1/AlbumGenerator.java
+++ app/src/main/java/onedv502_a1/AlbumGenerator.java
@@ -1,8 +1,47 @@
 package onedv502_a1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Javadoc comment.
  */
 public class AlbumGenerator {
+  private int releaseYear;
+  private String name;
+  private String artist;
+
+  public AlbumGenerator(String name, int releaseYear, String artist) {
+    this.name = name;
+    this.releaseYear = releaseYear;
+    this.artist = artist;
+  }
+
+  public void generatAlbums() {
+    List<String> list = new ArrayList<>();
+    list.add("Dark Side of the moon ");
+    list.add("Wish you were here ");
+    list.add("Animals ");
+    list.add("The Wall ");
+    String[] fourAlbums = new String[list.size()]; // create array of the same size as the list
+    for (int i = 0; i < fourAlbums.length; i++) {
+      fourAlbums[i] = list.get(i);
+
+    }
+
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getArtist() {
+    return artist;
+  }
+
+  public int getReleaseYear() {
+    return releaseYear;
+  }
+  
 
 }
