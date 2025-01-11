14dc5c6f2312fe7168ba4cb66e5e48ceb625520f
Abdifataah Abdillahi Ali <abdifatahali22@gmail.com>
2025-01-11 15:00:52 +0100

    Add way to calc average and update gradle settings

--- app/src/main/java/onedv502_a1/ArrayAlgorithms.java
+++ app/src/main/java/onedv502_a1/ArrayAlgorithms.java
@@ -5,6 +5,21 @@ package onedv502_a1;
  * Javadoc comment.
  */
 public class ArrayAlgorithms {
+
+  int[] values = {5, 2, 6, 4, 1, 3, 7, 9, 8};
+
+  public double average(int[] averageValue) {
+    int sum = 0;
+    int lenght = values.length;
+
+    for (int i = 0; i < values.length; i++) {
+      sum += values[i];
+    }
+    double avg = sum / lenght;
+    return avg;
+
+  }
+   
  
 
 }
